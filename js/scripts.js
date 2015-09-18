369525cd533fb95d7485d4683f1b909da42dd7ee
chris-swan <chris@chrisswan.com>
2015-09-18 11:43:42 -0700

    add css files to start modifying, all basic functionality is working now.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,26 @@
+
+//Pizza function:
 function Pizza(topping, pizzaSize, quantity) {
   this.topping = topping;
   this.pizzaSize = pizzaSize;
   this.quantity = quantity;
 };
 
+
+//Calcuate price: start with base price, modify based on selections made:
 Pizza.prototype.calculatePrice = function() {
   var basePrice = 10;
   var toppingType = this.topping;
   var toppingArray = ["Pepperoni", "BBQ Chicken", "Veggie", "Vegan", "Garlic special", "Hawaiian", "Meat Lovers", "Pizza Especial"];
-  
+ 
+ //using an array of pizzas, add additional cost based on topping selection: 
 if (toppingArray.indexOf(toppingType) != -1) {
     var toppingPrice = basePrice + 5;
   } else {
     var toppingPrice = basePrice;
   }
 
+//Adjust price based on size:
 if (this.pizzaSize == "Large") {
   var sizePrice = toppingPrice + 10;
 } else if (this.pizzaSize == "Medium") {
@@ -23,12 +29,14 @@ if (this.pizzaSize == "Large") {
   var sizePrice = toppingPrice;
 }
 
+//Adjust the price based on the quantity selected:
 var PizzaPrice = this.quantity * sizePrice;
 
 return PizzaPrice;
 
 }
 
+//link the form to the JavaScript to creat price:
 $(document).ready(function() {
 
   $("form#pizza-selection").submit(function(event) {
@@ -45,7 +53,6 @@ $(document).ready(function() {
 
     $(".price").text("$" + PizzaPrice);
 
-
-});
+  });
 
 });
