9cf9547a3a9033ab0090ca4cb9a5226f65b3b5b8
chris-swan <chris@chrisswan.com>
2015-09-18 09:54:39 -0700

    pass first test to create pizza function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-
+function Pizza(pizzaSize, topping, quantity) {
+  this.pizzaSize = pizzaSize;
+  this.topping = topping;
+  this.quantity = quantity;
+};
--- spec/specs.js
+++ spec/specs.js
@@ -1,1 +1,9 @@
-specs
+describe('Pizza', function() {
+  it("Retrieves pizza size, topping selection and quantity", function() {
+    var testPizza = new Pizza("Large", "BBQ chicken", 2);
+    expect(testPizza.pizzaSize).to.equal("Large");
+    expect(testPizza.topping).to.equal("BBQ chicken");
+    expect(testPizza.quantity).to.equal(2);
+  });
+
+});
