33048656c0885d75cae10a4301866fa63ecfdd84
chris-swan <chris@chrisswan.com>
2015-09-18 10:12:56 -0700

    pass test to assign ign base price from topping selection.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
-function Pizza(pizzaSize, topping, quantity) {
-  this.pizzaSize = pizzaSize;
+function Pizza(topping, pizzaSize, quantity) {
   this.topping = topping;
+  this.pizzaSize = pizzaSize;
   this.quantity = quantity;
 };
+
+Pizza.prototype.calculatePrice = function() {
+  var basePrice = 10;
+  var toppingType = this.topping;
+  var toppingArray = ["Pepperoni", "BBQ Chicken", "Veggie", "Vegan"];
+  
+if (toppingArray.indexOf(toppingType) != -1) {
+    var toppingPrice = basePrice + 3;
+  } else {
+    var toppingPrice = basePrice;
+  }
+
+  return toppingPrice;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,13 @@
 describe('Pizza', function() {
   it("Retrieves pizza size, topping selection and quantity", function() {
-    var testPizza = new Pizza("Large", "BBQ chicken", 2);
-    expect(testPizza.pizzaSize).to.equal("Large");
+    var testPizza = new Pizza("BBQ chicken", "Large", 1);
     expect(testPizza.topping).to.equal("BBQ chicken");
-    expect(testPizza.quantity).to.equal(2);
+    expect(testPizza.pizzaSize).to.equal("Large");
+    expect(testPizza.quantity).to.equal(1);
   });
-
+  //Test to assign base price from pizza.
+it("assigns a price based on topping", function () {
+    var testPizza = new Pizza("cheese", "Small", 1);
+    expect(testPizza.calculatePrice()).to.equal(10);
+  });
 });
